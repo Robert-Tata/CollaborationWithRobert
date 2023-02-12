c44c56babcc4b19fe76827b573e0b13469917359
Ted Lehr <t_l81@txstate.edu>
2023-02-12 12:43:37 -0600

    Simple Hello World File

---
+++ HelloWithIncrements.java
@@ -0,0 +1,23 @@
+// SJ's comment
+//This comment is from Shawn
+
+// Inclass demo comment
+
+public class HelloWithIncrements {
+    public static void main(String[] args) {
+        int number = 0;
+
+        System.out.println("Hello, World");
+        number++;
+        number = number + number++;
+        number = new HelloWithIncrements().incrementByTwo(number);
+        number--;
+        System.out.println("Final number: " + number);
+        System.out.println("Goodbye, World");
+    }
+
+    public int incrementByTwo(int number) {
+        return(number + 2);
+    }
+    //Danger comment  
+}
